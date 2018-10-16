517a8fd7eb69ebc2384fa6166b58cf9067c3547b
JoshuaBaert <developer@baert.io>
2018-10-16 10:07:07 -0500

    updated for multiple machines

--- _profile.ps1
+++ _profile.ps1
@@ -1,18 +1,37 @@
+$scriptsDir = 'C:\Users\Joshua\scripts' 
+
+# To get this file working you need to have the folowing file :
+# C:\WINDOWS\System32\WindowsPowerShell\v1.0\profile.ps1
+# then add this line :
+# Import-Module C:\path\to\_profile.ps1
+
+# Required config Vars
+# $excludedDirs
+# $preferedDir
+import-Module $scriptsDir\config.ps1
+
+# Import Extra Modules 
+Import-Module $scriptsDir\colorsEtc.ps1
+
 # Vars
 $test = 'Yep.. Working!'
-$preferedDir = 'C:\git\work'
-$scriptsDir = 'C:\Users\uc249159'
 
-# Check
+# Check working director and maybe redirect
 $dir = pwd
-switch -Regex ($dir) {
-    '^C\:\\$' { set-location $preferedDir }
-    '^H\:\\$' { set-location $preferedDir }
-    Default {}
+
+write $excludedDirs
+if ($excludedDirs -contains $dir){
+    Set-Location $preferedDir
 }
 
+# switch -Regex ($dir) {
+#     '^C\:\\$' { set-location $preferedDir }
+#     '^H\:\\$' { set-location $preferedDir }
+#     Default {}
+# }
+
 $prefix = 'g'
-Get-ChildItem "$scriptsDir\ps-scripts\git" -Filter *.ps1 | 
+Get-ChildItem "$scriptsDir\git" -Filter *.ps1 | 
 Foreach-Object {
     if ($_ -notlike '_profile.ps1') {
         $alias = $_ -replace ".ps1",""
---
+++ colorsEtc.ps1
@@ -0,0 +1,46 @@
+# u can see all current colors by running 
+# Get-PSReadlineOption
+# and setting with 
+# Set-PSReadlineOption
+
+$console = $host.ui.rawui
+
+$console.backgroundcolor = "Black"
+$console.foregroundcolor = "White"
+
+$colors = $host.privatedata
+
+$colors.VerboseForegroundColor = "White"
+$colors.ProgressForegroundColor = "White"
+$colors.DebugForegroundColor = "White"
+$colors.WarningForegroundColor = "Yellow"
+$colors.ErrorForegroundColor = "Red"
+
+$colors.VerboseBackgroundColor = "Black"
+$colors.ProgressBackgroundColor = "Black"
+$colors.DebugBackgroundColor = "Black"
+$colors.WarningBackgroundColor = "Black"
+$colors.ErrorBackgroundColor = "Black"
+
+Set-PSReadlineOption -TokenKind Comment -ForegroundColor DarkGreen
+Set-PSReadlineOption -TokenKind Keyword -ForegroundColor Green
+Set-PSReadlineOption -TokenKind String -ForegroundColor Yellow
+Set-PSReadlineOption -TokenKind Operator -ForegroundColor Red
+Set-PSReadlineOption -TokenKind Variable -ForegroundColor Blue
+Set-PSReadlineOption -TokenKind Command -ForegroundColor Yellow
+Set-PSReadlineOption -TokenKind Parameter -ForegroundColor Cyan
+Set-PSReadlineOption -TokenKind Type -ForegroundColor Magenta
+Set-PSReadlineOption -TokenKind Number -ForegroundColor White
+Set-PSReadlineOption -TokenKind Member -ForegroundColor White
+
+Set-PSReadlineOption -TokenKind Comment -BackgroundColor Black
+Set-PSReadlineOption -TokenKind Keyword -BackgroundColor Black
+Set-PSReadlineOption -TokenKind String -BackgroundColor Black
+Set-PSReadlineOption -TokenKind Operator -BackgroundColor Black
+Set-PSReadlineOption -TokenKind Variable -BackgroundColor Black
+Set-PSReadlineOption -TokenKind Command -BackgroundColor Black
+Set-PSReadlineOption -TokenKind Parameter -BackgroundColor Black
+Set-PSReadlineOption -TokenKind Type -BackgroundColor Black
+Set-PSReadlineOption -TokenKind Number -BackgroundColor Black
+Set-PSReadlineOption -TokenKind Member -BackgroundColor Black
+
