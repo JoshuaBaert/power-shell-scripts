37702e8d38982b1f321c6dc9c1267355462eca22
Joshua Baert <Joshua.Baert@thomsonreuters.com>
2018-11-02 11:07:26 -0500

    =)

--- git/mm.ps1
+++ git/mm.ps1
@@ -2,19 +2,19 @@ $message = git status
 
 if($message -like '*nothing to commit, working tree clean*') {    
     git checkout master
-    $pullMessage = git pull
+    git pull
     git checkout -
-    git merge master
+    $mergeMessage = git merge master
 
     $status = git status
     if($status -like '*Your branch is ahead of*' -And $status -like '*Your branch is ahead of*') {
         Write-Host 'Pushing the merge up'
         git push
-    }
 
-    if($pullMessage -imatch 'package\.json\s*\|\s*\d*' ){
-        Write-Host 'Installing new packages'
-        npm install
+        if($mergeMessage -imatch 'package\.json\s*\|\s*\d*' ){
+            Write-Host 'Installing new packages'
+            npm install
+        }
     }
 } else {
     Write-Warning 'Not working on a clean branch or not in a git directory'
