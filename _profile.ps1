192df6f86f6a46dcda9784a18db09f145dc6a367
Baert <Joshua.Baert@thomsonreuters.com>
2018-11-08 17:01:01 -0600

    =)

--- _profile.ps1
+++ _profile.ps1
@@ -22,7 +22,6 @@ Get-ChildItem "$scriptsDir" -Directory | ForEach-Object {
     Get-ChildItem $_.FullName -Filter *.ps1 | Foreach-Object {
         if ($_ -notlike '_profile.ps1') {
             $alias = $_ -replace ".ps1",""
-            write "$prefix$alias"
             New-Alias "$prefix$alias" $_.FullName
         } elseif($_ -like '_profile.ps1') {
             Import-Module $_.FullName
