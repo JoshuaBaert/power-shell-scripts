fb974ec521dd3d2e4e635891d42cadb604cdc8fe
Joshua Baert <Joshua.Baert@thomsonreuters.com>
2019-01-23 11:22:54 -0600

    save

--- scripts/chocolatey/_profile.ps1
+++ scripts/chocolatey/_profile.ps1
@@ -1,6 +1,4 @@
 function chls { choco list --local-only }
 function chin { 
-    $str = ''
-    for ($i = 0; $i -lt $args.Count; $i++) { $str += " " + $args[$i] }
-    choco install -y $str
+    choco install -y $args
 } 
