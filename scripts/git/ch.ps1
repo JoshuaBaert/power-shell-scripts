bd8538b890e7844c4db53825538951939964f770
JoshuaBaert <developer@baert.io>
2019-01-25 11:08:41 -0600

    save

--- scripts/git/ch.ps1
+++ scripts/git/ch.ps1
@@ -1,1 +1,9 @@
-git checkout $args
+$branchName = $args[0]
+$branches = git branch
+
+if($branches -like "*$branchName*") {
+    git checkout $args
+} else {
+    git fetch
+    git checkout $args
+}
