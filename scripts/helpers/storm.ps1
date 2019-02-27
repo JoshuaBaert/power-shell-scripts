7cae9496c797d3418344fc0e190d14ae6595dfa9
Joshua Baert <Joshua.Baert@thomsonreuters.com>
2019-02-27 13:28:44 -0600

    figured out webstorm

--- scripts/helpers/storm.ps1
+++ scripts/helpers/storm.ps1
@@ -1,17 +1,2 @@
 
-$appDir = "$env:USERPROFILE\AppData\Local\JetBrains\Toolbox\apps\WebStorm\ch-0"
-$versions = @()
-
-Get-ChildItem $appDir -Directory | ForEach-Object {
-    $versions += $_.Name
-}
-
-$versions = $versions | Sort-Object -Descending
-$version = $versions[0]
-
-if ($args) {
-#     Start-Process "$appdir\$version\bin\webstorm.bat" $args
-} else {
-    # $currenLocation = Get-Location
-    # Start-Process "$appdir\$version\bin\webstorm.bat" $currenLocation
-}
+webstorm $args
