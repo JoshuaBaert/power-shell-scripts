56376934265b6d73dce0dfb25cd45c264e05d6b3
Joshua Baert <Joshua.Baert@thomsonreuters.com>
2019-03-04 16:50:22 -0600

    save

--- events/helpers.ps1
+++ events/helpers.ps1
@@ -5,9 +5,9 @@ $location = Split-Path -Path $PSScriptRoot -Parent
 if (!(Test-Path "$location\logs")) { md "$location\logs" }
 
 function logOut () {
-    $args | Out-File -Append "$location\logs\$logName-log.txt"
+    $args | Out-File -Encoding unicode -Append "$location\logs\$logName-log.txt"
 }
 
 $logBlock = {
-    $args | Out-File -Append "$location\logs\$logName-log.txt"
+    $args | Out-File -Encoding unicode -Append "$location\logs\$logName-log.txt"
 }
