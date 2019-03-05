b42e671eda6354db62589351eacc610b3c09a965
Joshua Baert <Joshua.Baert@thomsonreuters.com>
2019-03-05 09:39:13 -0600

    save

--- scripts/setup/-git-posh.ps1
+++ scripts/setup/-git-posh.ps1
@@ -1,13 +1,6 @@
-# Setup
+
 $startingDir = pwd
 $repoUrl = 'https://github.com/dahlbyk/posh-git.git'
-
-
-function profile {
-
-}
-
-# Execution
 
 if (!(Test-Path 'C:\tools')) {
     mkdir C:\tools
@@ -18,9 +11,8 @@ Set-Location C:\tools
 if (!(Test-Path 'C:\tools\posh-git')) {
     # install post-git
     git clone $repoUrl
-
-} else {Write-Warning "Posh Git already exists" }
-
-
+} else {
+    Write-Warning "Posh Git already exists"
+}
 
 Set-Location $startingDir
