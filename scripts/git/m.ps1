61541a983e0ef03e67bab24fa06f0e4f4c26e8ec
Joshua Baert <developer@baert.io>
2019-04-29 17:00:52 -0500

    save

--- scripts/git/m.ps1
+++ scripts/git/m.ps1
@@ -1,6 +1,7 @@
 param (
     [switch] $noPush = $false,
-    [switch] $noInstall = $false
+    [switch] $noPull = $false,
+    [switch] $noInstallNode = $false
 )
 
 $branchName = $args[0]
@@ -13,19 +14,22 @@ if ($branchName -eq $null) {
 $message = git status
 
 if ($message -like '*nothing to commit, working tree clean*') {
-    git checkout $branchName
-    git pull
-    git checkout -
-    $mergeMessage = git merge $branchName
+    if ($noPull) {
+        git checkout $branchName
+        git pull
+        git checkout -
+    }
 
+    $mergeMessage = git merge $branchName
     $status = git status
+
     if ($status -like '*Your branch is ahead of*' -And $status -like '*Your branch is ahead of*') {
         if (!$noPush) {
             Write-Host 'Pushing the merge up'
             git push
         }
 
-        if ($mergeMessage -imatch 'package\.json\s*\|\s*\d*' -and !$noInstall ){
+        if ($mergeMessage -imatch 'package\.json\s*\|\s*\d*' -and !$noInstallNode ){
             Write-Host 'Installing new packages'
             npm install
         }
