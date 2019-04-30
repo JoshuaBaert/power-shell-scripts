920a6b736e204c5dc39784c2aa4c12f6e1bce5c4
Joshua Baert <developer@baert.io>
2019-04-30 10:11:06 -0500

    save

--- scripts/chocolatey/-node-8.ps1
+++ scripts/chocolatey/-node-8.ps1
@@ -1,9 +1,9 @@
 
-function install { choco install -y nodejs --version 8.11.4 }
+function install { choco install -y nodejs --version 8.15.1 }
 function installNew { choco install -y nodejs }
 
 $list = choco list --local-only
-if ($list -like '*nodejs*8.11.4*') {
+if ($list -like '*nodejs*8.15.1*') {
     choco uninstall -y nodejs
     installNew
 } elseif ($list -like '*nodejs*') {
