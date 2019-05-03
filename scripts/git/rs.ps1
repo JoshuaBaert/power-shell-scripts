52a09907ef0e81be629ac3a7c1010c1917b8f78b
Joshua Baert <developer@baert.io>
2019-05-03 15:48:52 -0500

    updated sqash

--- scripts/git/rs.ps1
+++ scripts/git/rs.ps1
@@ -1,8 +1,12 @@
-
-
 $branchName = $args[0]
 $commitMessage = $args[1]
 $status = git status
+
+if ($status -like "*nothing to commit*") {
+} else {
+    Write-Error 'Not working on a clean branch'
+    exit
+}
 
 if ($branchName -eq $null) {
     Write-Warning 'You need a branch name as first argument'
@@ -18,29 +22,24 @@ if ([string]::IsNullOrEmpty($commitMessage)) {
     exit
 }
 
-if ($status -like '*nothing to commit, working tree clean*') {
-    Write-Error 'Not working on a clean branch'
-    exit
+Write-Warning "Have you already merged $branchName (Y or N)"
+
+$keyOption = 'Y','N'
+while ($keyOption -notcontains $keyPress.Character) {
+    $keyPress = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+}
+
+switch ($keyPress.Character) {
+    Y { $canSquash = $true }
+    N { $canSquash = $false }
+    Default { $canSquash = $false }
+}
+
+if ($canSquash) {
+    Write-Warning "Squashing Branch!! & Pushing forcefully"
+    git reset --soft $branchName
+    git commit -m $commitMessage
+    git push -f
 } else {
-    Write-Warning "Have you already merged $branchName (Y or N)"
-
-    $keyOption = 'Y','N'
-    while ($keyOption -notcontains $keyPress.Character) {
-        $keyPress = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-    }
-
-    switch ($keyPress.Character) {
-        Y { $canSquash = $true }
-        N { $canSquash = $false }
-        Default { $canSquash = $false }
-    }
-
-    if ($canSquash) {
-        Write-Warning "Squashing Branch!! & Pushing forcefully"
-        git reset --soft $branchName
-        git commit -m $commitMessage
-        git push -f
-    } else {
-        Write-Warning "Did NOT squash your branch"
-    }
+    Write-Warning "Did NOT squash your branch"
 }
