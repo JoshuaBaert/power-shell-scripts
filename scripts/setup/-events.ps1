af726869c2f3254075352d3eef97a61bb73c7def
Thor Baert <jbaert@nuskin.com>
2020-01-21 15:39:28 -0700

    updated comments

--- scripts/setup/-events.ps1
+++ scripts/setup/-events.ps1
@@ -1,3 +1,4 @@
+# Add to user logon and logoff via group policy editor
 
 $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
 if (!($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))){
